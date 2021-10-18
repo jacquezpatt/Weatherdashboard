b5089ff6211c158a3ba1fc2cbf8089a163d2e415
Jacquez Patterson <jacquezpatt@gmail.com>
2021-10-18 10:42:44 -0400

    current city weather

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
-var forecast= "bcca4d036d363451e0d97d2838d5f8fc"
-//https://api.openweathermap.org/data/2.5/weather?q=charlotte&appid=bcca4d036d363451e0d97d2838d5f8fc
-//https://api.openweathermap.org/data/2.5/forecast?q={city name}&appid=
+var forecast = "bcca4d036d363451e0d97d2838d5f8fc"
+var cityTemp = document.getElementById("cityTemp")
+var humidityElement = document.getElementById("humidity")
+var windspeedElement = document.getElementById("windspeed")
+var uv = document.getElementById("uv")
+var searchBtn = document.querySelector(".search-btn")
+var cityInput = document.querySelector(".cityInput")
 
 function getApi() {
-    // replace `octocat` with anyone else's GitHub username
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=charlotte&appid=bcca4d036d363451e0d97d2838d5f8fc';
-  
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data)
-      });
-  }
+  var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=charlotte&units=imperial&appid=bcca4d036d363451e0d97d2838d5f8fc';
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var cityName = data.name
+      var temp = data.main.temp
+      var humidity = data.main.humidity
+      var windspeed = data.wind.speed
+  cityTemp.textContent = cityName + ", " + temp + " F"
+humidityElement.textContent = "Humidity: " + humidity + "%"
+windspeedElement.textContent =  "Wind Speed: " + windspeed + "mph"
+    });
+}
+
+searchBtn.addEventListener("click", function() {
+  event.preventDefault()
   getApi()
+  });
+
+
